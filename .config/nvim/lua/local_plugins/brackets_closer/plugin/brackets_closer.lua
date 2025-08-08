0d32bb4fb0cf9b2584749a68254969e9af5bf9af
Ritsu <s1f102503183@iniad.org>
2025-08-08 10:02:51 +0900

    add the self made plugin

---
+++ .config/nvim/lua/local_plugins/brackets_closer/plugin/brackets_closer.lua
@@ -0,0 +1,16 @@
+vim.api.nvim_create_autocmd("InsertChatPre",{
+
+	pattern = {"("}
+
+	callback function(args)
+
+		local char = args.data.char
+
+		if char == "(" then
+			local col = vim.api.nvim_get_mode().col
+			local line =  vim.api.nvim_get_current_line()
+			-- カーソルの後ろに　） を挿入
+			vim.api.nvim_set_text( 0, 0, col, 0, col, {")"})
+		end
+	end
+}
