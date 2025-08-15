508069f44f4b4d8fe6d00c32cfa841c908f5d488
和田律 <wadaritsu@wadatadashinoMacBook-Air.local>
2025-08-15 23:27:14 +0900

    add auto close parentheses

--- .config/nvim/lua/plugins/neotest.lua
+++ .config/nvim/lua/plugins/neotest.lua
@@ -11,7 +11,31 @@ return {
                 "rcasia/neotest-java",
             },
 
-            opts = { adapters = { "neotest-java" }, },
+                        opts = {
+              -- Can be a list of adapters like what neotest expects,
+              -- or a list of adapter names,
+              -- or a table of adapter names, mapped to adapter configs.
+              -- The adapter will then be automatically loaded with the config.
+              adapters = { "neotest-java" },
+              -- Example for loading neotest-golang with a custom config
+              -- adapters = {
+              --   ["neotest-golang"] = {
+              --     go_test_args = { "-v", "-race", "-count=1", "-timeout=60s" },
+              --     dap_go_enabled = true,
+              --   },
+              -- },
+              status = { virtual_text = true },
+              output = { open_on_run = true },
+              quickfix = {
+                open = function()
+                  if LazyVim.has("trouble.nvim") then
+                    require("trouble").open({ mode = "quickfix", focus = false })
+                  else
+                    vim.cmd("copen")
+                  end
+                end,
+              },
+            }
             
         },
 }
