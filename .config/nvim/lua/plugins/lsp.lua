545dd752e54ee1a9d9eb7a87eb33c4f267f73f0c
Ritsu <s1f102503183@iniad.org>
2025-09-05 02:52:27 +0900

    java,rust,luaのlspとフォーマット機能を入れた。ts,pyはnpmがいる

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -1,33 +1,47 @@
--- lua/plugins/lsp.lua (修正版)
 return {
-  "neovim/nvim-lspconfig",
-  dependencies = {
-    -- 補完機能
-    "hrsh7th/nvim-cmp",
-    "hrsh7th/cmp-nvim-lsp",
-    
-    -- LSPサーバー管理
-    "williamboman/mason.nvim",
-    "williamboman/mason-lspconfig.nvim",
-  },
-  config = function()
-    -- ステップ1: まずmason.nvimをセットアップする
-    require("mason").setup()
-    
-    -- ステップ2: 次にmason-lspconfig.nvimをセットアップする
-    require("mason-lspconfig").setup()
-    
-    -- ここに各言語のLSP設定を記述する
-    local lspconfig = require("lspconfig")
-    
-    -- 例: RustのLSPを有効化
-    lspconfig.rust_analyzer.setup({})
-    
-    -- 例: TypeScriptのLSPを有効化
-    lspconfig.tsserver.setup({})
+    "neovim/nvim-lspconfig",
+    dependencies = {
+        -- 補完機能
+        "hrsh7th/nvim-cmp",
+        "hrsh7th/cmp-nvim-lsp",
 
-    -- JavaのLSP設定をここに追加
-    lspconfig.jdtls.setup({})
+        -- LSPサーバー管理
+        "williamboman/mason.nvim",
+        "williamboman/mason-lspconfig.nvim",
+    },
+    config = function()
+        -- ステップ1: まずmason.nvimをセットアップする
+        require("mason").setup({
+            ui = {
+                icons = {
+                    package_installed = "✓",
+                    package_pending = "➜",
+                    package_uninstalled = "✗"
+                }
+            }
+        })
 
-  end,
-}
+        -- ステップ2: 次にmason-lspconfig.nvimをセットアップする
+        require("mason-lspconfig").setup()
+
+        -- LSP設定の共通部分を定義
+        local lspconfig = require("lspconfig")
+        local on_attach = function(client, bufnr)
+            -- フォーマット機能
+            if client.supports_method("textDocument/formatting") then
+                vim.api.nvim_create_autocmd("BufWritePre", {
+                    group = vim.api.nvim_create_augroup("LspFormatting", {}),
+                    buffer = bufnr,
+                    callback = function()
+                        vim.lsp.buf.format({ async = true })
+                    end,
+                })
+            end
+        end
+
+        -- 各言語のLSPサーバーを有効化
+        lspconfig.rust_analyzer.setup({ on_attach = on_attach })
+        lspconfig.pyright.setup({ on_attach = on_attach })
+        lspconfig.tsserver.setup({ on_attach = on_attach })
+        lspconfig.lua_ls.setup({ on_attach = on_attach })
+    end, }
