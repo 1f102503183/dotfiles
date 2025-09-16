5d31c72bab3e8aac52aed4f745ec815e46ff4672
Ritsu <s1f102503183@iniad.org>
2025-09-16 17:13:14 +0900

    treesitter is not active

---
+++ .config/nvim/lua/plugins/comment.nvim.lua
@@ -0,0 +1,8 @@
+return {
+    "numToStr/Comment.nvim",
+    opts = {}
+}
+
+-- some mapping
+--`gc` - Toggles the region using linewise comment
+-- `gb` - Toggles the region using blockwise comment
--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,14 +1,15 @@
-return {
+return { {
     "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
     config = function()
-        require "nvim-treesitter.configs".setup {
+        require("nvim-treesitter.configs").setup({
             ensure_installed = { "lua", "markdown", "css", "bash", "html", "java", "python", "rust", "typescript", "javascript" },
-            sync_install = false,
+            sync_install = true,
             auto_install = true,
-            highlight = {
-                enable = true,
-            },
-        }
+            highlight = { enable = true },
+            indent = { enable = true },
+            textobjects = { select = { enable = true }, },
+        })
     end,
+},
 }
