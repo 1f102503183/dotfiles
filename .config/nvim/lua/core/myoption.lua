0c295e6e2b8b0a30fa39c1c5f8db3e2e0faf9d42
Ritsu <s1f102503183@iniad.org>
2025-10-06 19:18:54 +0900

    文法ミスを消した

--- .config/nvim/lua/core/myoption.lua
+++ .config/nvim/lua/core/myoption.lua
@@ -6,16 +6,3 @@ vim.opt.expandtab = true
 vim.opt.wrap = false
 
 vim.keymap.set("n", "<C-n>", ":NvimTreeToggle<CR>", { silent = true })
-vim.keymap.set('n', '<leader>b', function()
-    -- OSによってコマンドを切り替え
-    local cmd
-    if vim.fn.has("mac") == 1 then
-        cmd = 'open'
-    elseif vim.fn.has("win32") == 1 then
-        cmd = 'explorer'
-    else
-        cmd = 'xdg-open'
-    end
-    -- コマンドを実行
-    vim.cmd('!'.cmd .. ' ' .. vim.fn.expand('%:p') .. ' &')
-end, { noremap = true, silent = true, desc = 'Open current file in browser' })
